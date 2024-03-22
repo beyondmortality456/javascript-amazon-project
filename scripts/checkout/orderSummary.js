d89bbecdaf7ccdea26ef4f9ce2948ceb7750549d
beyondmortality456 <mariamrudenko@gmail.com>
2024-03-21 22:08:54 -0400

    Make payment summary interactice

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -9,13 +9,11 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
-
-const today = dayjs();
-const deliveryDate = today.add(7, "days");
-console.log(deliveryDate.format("dddd, MMMM D"));
-
-hello();
+import {
+  deliveryOptions,
+  getDeliveryOption,
+} from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
@@ -133,11 +131,11 @@ export function renderOrderSummary() {
     link.addEventListener("click", () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-
       const container = document.querySelector(
         `.js-cart-item-container-${productId}`
       );
       container.remove();
+      renderPaymentSummary();
       updateCheckoutQuantity();
     });
   });
@@ -193,6 +191,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }

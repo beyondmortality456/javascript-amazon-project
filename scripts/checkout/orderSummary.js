0181b88aaca88121585d97331eb55f6581dbb976
beyondmortality456 <mariamrudenko@gmail.com>
2024-03-25 23:29:19 -0400

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -33,6 +33,7 @@ export function renderOrderSummary() {
 
     cartSummaryHTML += `
       <div class="cart-item-container
+        js-cart-item-container
         js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: ${dateString}
@@ -49,7 +50,8 @@ export function renderOrderSummary() {
             <div class="product-price">
               $${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity
+            js-product-quantity-${matchingProduct.id}">
               <span>
                   Quantity: <span class="quantity-label js-quantity-label-${
                     matchingProduct.id
@@ -67,9 +69,9 @@ export function renderOrderSummary() {
                 data-product-id="${matchingProduct.id}">
                 Save
               </span>
-                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+                <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${
                   matchingProduct.id
-                }">
+                }" data-product-id="${matchingProduct.id}">
                   Delete
               </span>
             </div>
@@ -131,8 +133,7 @@ export function renderOrderSummary() {
       removeFromCart(productId);
       renderOrderSummary();
       renderPaymentSummary();
-      renderCheckoutHeader();
-      updateCheckoutQuantity();
+      // renderCheckoutHeader();
     });
   });
 
